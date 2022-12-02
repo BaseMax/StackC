59ccd30540ad5bf1622d31e7d08a66c810685ed0
Max Base <maxbasecode@gmail.com>
2022-12-02 18:36:24 +0100

    create .c and header file

---
+++ Stack.c
@@ -0,0 +1,13 @@
+/**
+ * 
+ * @file Stack.c
+ * @author Max Base (maxbasecode@gmail.com)
+ * @brief Stack Implementation in C
+ * @version 0.1
+ * @date 2022-12-02
+ * @copyright Copyright (c) 2022
+ * 
+ */
+
+#include "Stack.h"
+
---
+++ Stack.h
@@ -0,0 +1,25 @@
+/**
+ * 
+ * @file Stack.h
+ * @author Max Base (maxbasecode@gmail.com)
+ * @brief Stack Implementation in C
+ * @version 0.1
+ * @date 2022-12-02
+ * @copyright Copyright (c) 2022
+ * 
+ */
+
+#ifndef STACK_H
+#define STACK_H
+
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct Stack
+{
+    int top;
+    unsigned capacity;
+    int *array;
+} Stack;
+
+#endif
