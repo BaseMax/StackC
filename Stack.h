768472200761c6eb1c23601776ec686ff02d44fd
Max Base <maxbasecode@gmail.com>
2022-12-02 18:40:08 +0100

    adding functions to header

--- Stack.h
+++ Stack.h
@@ -23,4 +23,75 @@ typedef struct Stack
     int *items;
 } Stack;
 
+/**
+ * @brief Create a new stack
+ * @param size
+ * @return Stack* 
+ */
+Stack* newStack(int size);
+
+/**
+ * @brief Check if the stack is full
+ * @param stack
+ * @return true 
+ * @return false 
+ */
+bool isFull(Stack* stack);
+
+/**
+ * @brief Check if the stack is empty
+ * @param stack
+ * @return true 
+ * @return false 
+ */
+bool isEmpty(Stack* stack);
+
+/**
+ * @brief Push an item to stack
+ * @param stack
+ * @param item
+ */
+void push(Stack* stack, int item);
+
+/**
+ * @brief Pop an item from stack
+ * @param stack
+ * @return int 
+ */
+int pop(Stack* stack);
+
+/**
+ * @brief Peek the top item from stack
+ * @param stack
+ * @return int 
+ */
+int peek(Stack* stack);
+
+/**
+ * @brief Print the stack
+ * @param stack
+ */
+void printStack(Stack* stack);
+
+/**
+ * @brief Destroy the stack
+ * @param stack
+ */
+void destroyStack(Stack* stack);
+
+/**
+ * @brief Convert the stack to string
+ * @param stack
+ * @return char* 
+ */
+char* stackToString(Stack* stack);
+
+/**
+ * @brief Resize the stack
+ * @param stack
+ * @param size
+ * @return Stack* 
+ */
+Stack* resizeStack(Stack* stack, int size);
+
 #endif
