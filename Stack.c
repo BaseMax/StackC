e9b8e0fb10c236ef5324c7179008d529f49d0cd9
Max Base <maxbasecode@gmail.com>
2022-12-02 18:39:24 +0100

    Adding more functions

--- Stack.c
+++ Stack.c
@@ -46,3 +46,97 @@ bool isEmpty(Stack* stack)
 {
     return stack->top == -1;
 }
+
+/**
+ * @brief Push an item to stack
+ * @param stack
+ * @param item
+ */
+void push(Stack* stack, int item)
+{
+    if (isFull(stack)) {
+        printf("Stack is full!\n");
+        return;
+    }
+    stack->items[++stack->top] = item;
+    printf("Pushed %d to stack\n", item);
+}
+
+/**
+ * @brief Pop an item from stack
+ * @param stack
+ * @return int 
+ */
+int pop(Stack* stack)
+{
+    if (isEmpty(stack)) {
+        printf("Stack is empty!\n");
+        return INT_MIN;
+    }
+    return stack->items[stack->top--];
+}
+
+/**
+ * @brief Peek the top item from stack
+ * @param stack
+ * @return int 
+ */
+int peek(Stack* stack)
+{
+    if (isEmpty(stack)) {
+        printf("Stack is empty!\n");
+        return INT_MIN;
+    }
+    return stack->items[stack->top];
+}
+
+/**
+ * @brief Print the stack
+ * @param stack
+ */
+void printStack(Stack* stack)
+{
+    printf("Stack: ");
+    for (int i = 0; i <= stack->top; i++) {
+        printf("%d ", stack->items[i]);
+    }
+    printf("\n");
+}
+
+/**
+ * @brief Destroy the stack
+ * @param stack
+ */
+void destroyStack(Stack* stack)
+{
+    free(stack->items);
+    free(stack);
+}
+
+/**
+ * @brief Convert the stack to string
+ * @param stack
+ * @return char* 
+ */
+char* stackToString(Stack* stack)
+{
+    char* str = (char*)malloc(100 * sizeof(char));
+    sprintf(str, "Stack: ");
+    for (int i = 0; i <= stack->top; i++) {
+        sprintf(str, "%s%d ", str, stack->items[i]);
+    }
+    return str;
+}
+
+/**
+ * @brief Resize the stack
+ * @param stack
+ * @param size
+ * @return Stack* 
+ */
+Stack* resizeStack(Stack* stack, int size)
+{
+    stack->size = size;
+    stack->items = (int*)realloc(stack->items, stack->size * sizeof(int));
+    return stack;
+}
